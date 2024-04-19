01286bfd88636c721750d6742ed2d906a093ffd3
Nikita Yarosh <yarosh.nv@yandex.ru>
2024-04-19 09:21:52 +0300

    feature: добавил отправку (из producer) информации об объекте их бд по его id (в consumer)

--- src/main/java/com/yarosh/rejaze/controller/Controller.java
+++ src/main/java/com/yarosh/rejaze/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.yarosh.rejaze.controller;
 
 import com.yarosh.rejaze.kafka.KafkaProducer;
+import com.yarosh.rejaze.repository.CatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,13 @@ public class Controller {
 
     private final KafkaProducer kafkaProducer;
 
+    private final CatRepository catRepository;
+
     @PostMapping("/kafka/send")
-    public String send(@RequestBody String message) {
-        kafkaProducer.sendMessage(message);
+    public String send(@RequestParam int id) {
+        var cat = catRepository.findById(id);
+
+        kafkaProducer.sendMessage(cat.toString());
 
         return "Success";
     }
